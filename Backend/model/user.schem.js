8d50ebeb639b6772cfa900d11ed04595a08fa41a
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2022-12-26 21:43:08 +0530

    made the methods in model by mongoose methods

--- Backend/model/user.schem.js
+++ Backend/model/user.schem.js
@@ -1,6 +1,8 @@
-import mongoose from "mongoose";
+import mongoose, { model } from "mongoose";
 import authRole from "../utils/authRole.js";
 import bcrypt from "bcryptjs"
+import config from "../config/index.js";
+import crypto from 'crypto'
 
 const userSchema= mongoose.Schema(
     {
@@ -52,3 +54,46 @@ userSchema.pre('save', async function(next){
 
 //mongoose Schema method 
 // add more feature directly to your schema
+
+userSchema.methods=
+{
+    //comparePassowrd
+    comparePassword: async function(enterpassowrd)
+    {
+        return await bcrypt.compare(enterpassowrd, this.password)
+    },
+
+    //generate the jwt token
+    getJwtToken: async function()
+    {
+       return JWT.sign(
+        {
+            _id:this.id,
+            role:this.role
+        },
+        config.JWT_SECRET,
+        {
+            expiresIn:config.JWT_EXPIRY
+        }
+
+       )
+    },
+    // generate the forgot Password token
+
+    generateForgotPasswordToken: function ()
+    {
+        const forgotToken=crypto.randomBytes(20).toString('hex')
+
+        // step-1 save to DB
+        this.forgetPasswordToken= crypto.
+        createHash('sha256')
+        .update(forgotToken)
+        .digest('hex')
+
+        this.forgotPasswordExpiry= Date.now() + 20 * 60 * 1000
+        // step 2- return Value to user
+        return forgetToken
+    }
+}
+
+export default mongoose.model("User",userSchema)
